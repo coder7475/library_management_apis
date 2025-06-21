320647fec07a19f035ff4c256cfedd502aa9ffde
coder7475 <robiulhossain7475@gmail.com>
2025-06-21 19:45:32 +0600

    refactor: Remove book and borrow schema files to streamline project structure and eliminate unused code

--- src/schemas/borrow.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.borrowZodSchema = void 0;
-const zod_1 = require("zod");
-exports.borrowZodSchema = zod_1.z.object({
-    book: zod_1.z.string(),
-    quantity: zod_1.z.number().int().positive(),
-    dueDate: zod_1.z.coerce.date(),
-});
